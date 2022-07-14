x-cast/obs-studio-sim

CI: Disable new mpegts output for linux
CI uses ubuntu 18.04 which is quite old. Support of the new mpegts output would require manual
 compilation of librist & libsrt libs.

Signed-off-by: pkv <pkv@obsproject.com>

CI/linux/02_build_obs.sh
@@ -58,6 +58,7 @@ _configure_obs() {
         -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} \
         -DLINUX_PORTABLE=${PORTABLE_BUILD:-OFF} \
         -DENABLE_AJA=OFF \
+        -DUSE_NEW_MPEGTS_OUTPUT=OFF \
         ${PIPEWIRE_OPTION} \
         ${YOUTUBE_OPTIONS} \
         ${TWITCH_OPTIONS} \

krakow10/obs-studio

CI/linux/02_build_obs.sh
@@ -58,6 +58,7 @@ _configure_obs() {
         -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} \
         -DLINUX_PORTABLE=${PORTABLE_BUILD:-OFF} \
         -DENABLE_AJA=OFF \
+        -DENABLE_NEW_MPEGTS_OUTPUT=OFF \
         ${PIPEWIRE_OPTION} \
         ${YOUTUBE_OPTIONS} \
         ${TWITCH_OPTIONS} \